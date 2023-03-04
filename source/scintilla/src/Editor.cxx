396bf40b4c5cc44438c95abf5f7a3cf10d909105
BoosterOfOld <michal.frdlik@hotmail.com>
2023-03-04 12:04:03 +0100

    .

--- source/scintilla/src/Editor.cxx
+++ source/scintilla/src/Editor.cxx
@@ -2977,6 +2977,84 @@ void Editor::ChangeCaseOfSelection(int caseMapping) {
 	}
 }
 
+/*
+#include <windows.h>
+#include <atlstr.h>
+//
+// Execute a command and get the results. (Only standard output)
+//
+CStringA ExecCmd(
+    const wchar_t* cmd              // [in] command to execute
+)
+{
+    CStringA strResult;
+    HANDLE hPipeRead, hPipeWrite;
+
+    SECURITY_ATTRIBUTES saAttr = {sizeof(SECURITY_ATTRIBUTES)};
+    saAttr.bInheritHandle = TRUE; // Pipe handles are inherited by child process.
+    saAttr.lpSecurityDescriptor = NULL;
+
+    // Create a pipe to get results from child's stdout.
+    if (!CreatePipe(&hPipeRead, &hPipeWrite, &saAttr, 0))
+        return strResult;
+
+    STARTUPINFOW si = {sizeof(STARTUPINFOW)};
+    si.dwFlags     = STARTF_USESHOWWINDOW | STARTF_USESTDHANDLES;
+    si.hStdOutput  = hPipeWrite;
+    si.hStdError   = hPipeWrite;
+    si.wShowWindow = SW_HIDE; // Prevents cmd window from flashing.
+                              // Requires STARTF_USESHOWWINDOW in dwFlags.
+
+    PROCESS_INFORMATION pi = { 0 };
+
+    BOOL fSuccess = CreateProcessW(NULL, (LPWSTR)cmd, NULL, NULL, TRUE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi);
+    if (! fSuccess)
+    {
+        CloseHandle(hPipeWrite);
+        CloseHandle(hPipeRead);
+        return strResult;
+    }
+
+    bool bProcessEnded = false;
+    for (; !bProcessEnded ;)
+    {
+        // Give some timeslice (50 ms), so we won't waste 100% CPU.
+        bProcessEnded = WaitForSingleObject( pi.hProcess, 50) == WAIT_OBJECT_0;
+
+        // Even if process exited - we continue reading, if
+        // there is some data available over pipe.
+        for (;;)
+        {
+            char buf[1024];
+            DWORD dwRead = 0;
+            DWORD dwAvail = 0;
+
+            if (!::PeekNamedPipe(hPipeRead, NULL, 0, NULL, &dwAvail, NULL))
+                break;
+
+            if (!dwAvail) // No data available, return
+                break;
+
+            if (!::ReadFile(hPipeRead, buf, min(sizeof(buf) - 1, dwAvail), &dwRead, NULL) || !dwRead)
+                // Error, the child process might ended
+                break;
+
+            buf[dwRead] = 0;
+            strResult += buf;
+        }
+    } //for
+
+    CloseHandle(hPipeWrite);
+    CloseHandle(hPipeRead);
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
+    return strResult;
+} //ExecCmd
+
+ */
+
+#if defined (__unix__) || (defined (__APPLE__) && defined (__MACH__))
+
 #define READ 0
 #define WRITE 1
 
@@ -3076,6 +3154,15 @@ std::string Editor::exec(const char* cmd, std::function<void(void)> func) {
     return result;
 }
 
+#else
+
+std::string Editor::exec(const char* cmd, std::function<void(void)> func) {
+	func();
+    return "Not defined on this platform.";
+}
+
+#endif
+
 void Editor::SystemExecuteSelection(std::function<void(void)> func, std::function<void(void)> onDone) {
 	UndoGroup ug(pdoc);
 	pdoc->BeginUndoAction();
@@ -3125,6 +3212,8 @@ void Editor::SystemExecuteSelection(std::function<void(void)> func, std::functio
 	}
 }
 
+#if defined (__unix__) || (defined (__APPLE__) && defined (__MACH__))
+
 void Editor::TerminateExecution(std::function<void(void)> func, std::function<void(void)> onDone)
 {
 	std::unique_lock<std::mutex> lock(this->execThreadMutex);
@@ -3134,6 +3223,16 @@ void Editor::TerminateExecution(std::function<void(void)> func, std::function<vo
 		kill(this->execThreadHandle, SIGKILL);
 	}
 }
+
+#else
+
+void Editor::TerminateExecution(std::function<void(void)> func, std::function<void(void)> onDone)
+{
+	func();
+	onDone();
+}
+
+#endif
 
 void Editor::LineTranspose() {
 	const Sci::Line line = pdoc->SciLineFromPosition(sel.MainCaret());
